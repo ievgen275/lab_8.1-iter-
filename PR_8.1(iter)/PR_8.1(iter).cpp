1ebe8aa3ccfedfacf9af9aa9657508b5b8d0f7f5
ievgen275 <ievgen3435@gmail.com>
2024-03-07 22:04:02 +0200

    first commit

---
+++ PR_8.1(iter)/PR_8.1(iter).cpp
@@ -0,0 +1,65 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int Count(char* str)
+{
+	for (int i = 0; str[i + 1] != 0; i++)
+		if ((str[i] == 's' && str[i + 1] == 'q') || 
+			(str[i] == 'q' && str[i + 1] == 's'))
+			return 1;
+	return 0;
+}
+
+char* Change(char* str)
+{
+    size_t len = strlen(str);
+    if (len < 3)
+        return str;
+
+    char* tmp = new char[len * 4 / 3 + 1];
+    char* t = tmp;
+    tmp[0] = '\0';
+    size_t i = 0;
+
+    while (i < len && str[i + 1] != 0) {
+        if ((str[i] == 's' && str[i + 1] == 'q') ||
+            (str[i] == 'q' && str[i + 1] == 's')) {
+            *t++ = '*';
+            *t++ = '*';
+            *t++ = '*';
+            i += 2;
+        }
+        else {
+            *t++ = str[i++];
+            *t = '\0';
+        }
+    }
+
+    *t++ = str[i++];
+    *t++ = str[i++];
+    *t = '\0';
+
+    return tmp;
+}
+
+int main()
+{
+    char str[101];
+
+    cout << "Enter string:" << endl;
+    cin.getline(str, 100);
+
+    cout << "String contained " << Count(str) << " groups of 'SQ or QS'" << endl;
+
+    char* dest = Change(str);
+
+    cout << "Modified string (param) : " << str << endl;
+    cout << "Modified string (result): " << dest << endl;
+
+    delete[] dest;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR_8.1(iter)/PR_8.1(iter).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[] = "abcsqdefg";
+			Assert::AreEqual(1, Count(str));
+		}
+
+		TEST_METHOD(TestMethod2)
+		{
+			char str[] = "sqabcsqdefgqsq";
+			char* result = Change(str);
+			Assert::AreEqual("***abc***defg***q", result);
+			delete[] result;
+		}
+	};
+}
